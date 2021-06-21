dcf0ff2450349bc33ee15ec02f62c46904de3206
gustavoafranca <gustavoavelarfranca@gmail.com>
2021-06-21 03:45:15 -0300

    Alterando o auth para criar

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -5,7 +5,7 @@ import Auth from "../middlewares/AuthMiddlewares";
 
 const userRoutes = Router();
 
-userRoutes.post('/create', Auth, UserController.create)
+userRoutes.post('/create', UserController.create)
 userRoutes.put('/update/:id', Auth, UserController.update)
 userRoutes.delete('/delete/:id', Auth, UserController.delete)
 userRoutes.get('/list', Auth, UserController.index)
